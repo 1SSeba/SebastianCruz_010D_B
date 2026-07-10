376fe981bc7f3bc8386fcf568a832ef23b3e60ea
1sseba <se.cruzd@duocuc.cl>
2026-07-10 10:17:59 -0400

    1

--- formaB.py
+++ formaB.py
@@ -99,13 +99,21 @@ def leer_op():
 
 def main():
     peliculas = {
-    'P101': ['Luz de Otoño', 'drama', 110, 'B', 'Español', False],
-    'P102': ['Noche Neón', 'acción', 125, 'C', 'Ingles', True],
+        'P101': ['Luz de Otoño', 'drama', 110, 'B', 'Español', False],
+        'P102': ['Noche Neón', 'acción', 125, 'C', 'Ingles', True],
+        'P103': ['Planeta Agua', 'documental', 90, 'A', 'Español', False],
+        'P104': ['Risa Total', 'comedia', 105, 'A', 'Español', True],
+        'P105': ['Código Zero', 'thriller', 118, 'C', 'Ingles', True],
+        'P106': ['Viaje Lunar', 'ciencia ficción', 132, 'B', 'Ingles', False]
     }
 
     cartelera = {
-    'P101': [5990, 40],
-    'P102': [7990, 0],
+        'P101': [5990, 40],
+        'P102': [7990, 0],
+        'P103': [4990, 25],
+        'P104': [6990, 12],
+        'P105': [8990, 8],
+        'P106': [7490, 3]
     }
 
     while True:
